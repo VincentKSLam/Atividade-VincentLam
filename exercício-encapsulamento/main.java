ac79a27f2d9be0d299add40ee639318cbed995fd
Vincent <vincentkasoenlam@gmail.com>
2026-09-11 13:29:40 +0000

    Add encapsulation and inheritance exercises

--- exercício-encapsulamento/main.java
+++ exercício-encapsulamento/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         cofrinho cofrasso = new cofrinho();
         cofrasso.setSaldo(0);
-        cofrasso.setObjetivo("sei lá");
+        cofrasso.setObjetivo("Viagem");
         
         cofrinho.ObjetivoSaldo dado = cofrasso.getObjetivoSaldo();
 
